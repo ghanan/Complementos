d41823b2bbaa0cf86e217a24f09050df15ca83f4
Antonio Tovar <antovar@gmail.com>
2020-11-18 21:14:28 +0100

    Comprobados parámetros

--- complementos.py
+++ complementos.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
 from os import listdir
+from sys import argv
 
 complementos = []
 comp_nombres = []
 productos = []
+lis_produc = []
+lis_elegidos = []
 
 def incorpora_producto(p):
     global productos
@@ -15,8 +18,6 @@ def incorpora_producto(p):
         try: kk = float(complemento[1]) + 0.0
         except: exit('Error: '+identificador[0]+' -> '+complemento[0]+': '+complemento[1])
     productos.append(producto)
-    print(productos)
-
 
 def lee_ficheros():
     global complementos, comp_nombres
@@ -27,10 +28,50 @@ def lee_ficheros():
     # ~ lista_productos = [f[:-8] for f in listdir(self.directorio) if f[-8:]=='-PIM.csv']
     # ~ productos=[l.split('|') for l in open('producto.txt').read().splitlines()]
 
+def lista_productos():
+    for p in productos:
+        print(p[0][2],p[0][1])
+
+def parametros_correctos(param):
+    for l in lis_elegidos:
+        if not l[0] in [p[0] for p in lis_produc]:
+            exit('Error: producto ' + l[0] + ' desconocido')
+            return False
+    for l in range(2, len(argv), 2):
+        if not argv[l] in ['0.5', '1', '1.5', '2', '2.5', '3']:
+            exit('Error: cantidad ' + argv[l] + ' en producto ' + argv[l-1])
+            return False
+
+def ordena_elegidos():
+    global lis_elegidos
+    _l = []
+    for l in lis_elegidos:
+        for p in productos:
+            if l[0] == p[0][0]: _l.append([l[0],len(p)])
+
+def rellena_listas_trabajo(param):
+    global lis_produc, lis_elegidos
+    for p in productos: lis_produc.append(p[0])
+    for l in range(1, len(argv), 2):
+        lis_elegidos.append([argv[l]])
+    print(lis_elegidos)
+
+def procesa(param):
+    ordena_elegidos()
+    pass
+
 def main():
     global complementos, productos
     lee_ficheros()
-    # ~ print(productos)
+    if len(argv) == 1: exit('Faltan argumentos (prod | producto num_unidades [...])')
+    if argv[1].lower() == 'prod':
+        lista_productos()
+        exit()
+    if len(argv) > 2:
+        rellena_listas_trabajo(argv[2:])
+        if parametros_correctos(argv[2:]): procesa(argv[2:])
+    else:
+        exit('Error en argumentos(producto num_unidades [...]')
 
 if __name__ == '__main__':
     main()
